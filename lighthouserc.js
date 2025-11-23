5badb05706a406941d0f0d39cf2b187035f715a5
Marcelo Costa <mcosta@board.com>
2025-11-23 10:21:00 +0100

    Updating lighthouse assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -9,12 +9,11 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     assert: {
-      preset: 'lighthouse:recommended',
       assertions: {
-        'categories:performance': ['warn', { minScore: 0.8 }],
+        'categories:performance': ['warn', { minScore: 0.9 }],
         'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['warn', { minScore: 0.9 }],
         'categories:seo': ['warn', { minScore: 0.9 }],
-        'csp-xss': 'off',
       },
     },
   },
